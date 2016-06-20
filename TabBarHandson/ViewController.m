ff1af33dc7661eb6844084b71cd3e5f5967cdabb
kanchanshyatoria <kanchanshyatoria@gmail.com>
2016-06-20 23:47:28 +0530

    Some UI changes.

--- TabBarHandson/ViewController.m
+++ TabBarHandson/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UiTabba>
 
 @end
 
@@ -23,6 +23,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
